f78cd625b088edad547be6a7ad449eece41be254
Stefan Hagen <stefan@hagen.link>
2023-07-02 18:47:11 +0200

    Derived some app specific variable values from module (well file) docstring and the place

--- kutoa/__init__.py
+++ kutoa/__init__.py
@@ -1,4 +1,7 @@
 """Weave (Finnish: kutoa) annotated json code examples with fill-ins from and to markdown prose."""
+import os
+import pathlib
+
 # [[[fill git_describe()]]]
 __version__ = '2023.6.21+parent.090d802f'
 # [[[end]]] (checksum: b2d5ee748ed63dc0bc63eda72fbc118d)
@@ -6,9 +9,17 @@ __version_info__ = tuple(
     e if '-' not in e else e.split('-')[0] for part in __version__.split('+') for e in part.split('.') if e != 'parent'
 )
 
-APP_NAME = 'Weave (Finnish: kutoa) annotated json code examples with fill-ins from and to markdown prose.'
-APP_ALIAS = 'kutoa'
+APP_ALIAS = str(pathlib.Path(__file__).parent.name)
 APP_ENV = APP_ALIAS.upper()
+APP_NAME = locals()['__doc__']
+DEBUG = bool(os.getenv(f'{APP_ENV}_DEBUG', ''))
+VERBOSE = bool(os.getenv(f'{APP_ENV}_VERBOSE', ''))
+QUIET = False
+STRICT = bool(os.getenv(f'{APP_ENV}_STRICT', ''))
+ENCODING = 'utf-8'
+ENCODING_ERRORS_POLICY = 'ignore'
+DEFAULT_CONFIG_NAME = f'.{APP_ALIAS}.json'
+
 VERSION = __version__
 VERSION_INFO = __version_info__
 
